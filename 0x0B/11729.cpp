97d533cc9920ee887bea75a9fa3aaef111d83228
k2hop1213 <140415177+k2hop1213@users.noreply.github.com>
2025-12-07 19:08:01 +0900

    Create 11729.cpp

---
+++ 0x0B/11729.cpp
@@ -0,0 +1,23 @@
+#include <stdc++.h>
+using namespace std;
+
+void func(int a, int b, long long int n) {
+	if (n == 1) { cout << a << ' ' << b << '\n'; return; }
+	func(a, 6 - a - b, n - 1);
+	cout << a << ' ' << b << '\n';
+	func(6 - a - b, b, n - 1);
+
+}
+
+int main() {
+
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	long long int k;
+	cin >> k;
+	cout << (1 << k) - 1 << '\n';
+	func(1, 3, k);
+
+
+	return 0;
+}
