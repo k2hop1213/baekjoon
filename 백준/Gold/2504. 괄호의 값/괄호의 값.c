7eaa3622a60976f750a203254b6835ec397d2dd6
KyuHyun Kim <140415177+k2hop1213@users.noreply.github.com>
2026-04-20 18:28:43 +0900

    [Gold V] Title: 괄호의 값, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/2504. 괄호의 값/괄호의 값.c
+++ 백준/Gold/2504. 괄호의 값/괄호의 값.c
@@ -1,56 +1,124 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
-char stack[31];
-int top = -1;
+#include <stdlib.h>
 
-int isEmpty() { return top == -1; }
+#define MAX_SIZE 101
 
-void push(char a) {
-	stack[++top] = a;
+char stack[MAX_SIZE];
+int top;
+
+void init_stack(void) {
+	top = -1;
 }
 
-char pop() {
-	if (isEmpty())return 0;
+int is_empty(void) {
+	return (top == -1);
+}
+
+int is_full(void) {
+	return (top == MAX_SIZE - 1);
+}
+
+void push(char item) {
+	if (!is_full()) {
+		stack[++top] = item;
+	}
+}
+
+char pop(void) {
+	if (is_empty()) {
+		return '\0';
+	}
 	return stack[top--];
 }
 
-int main() {
-	char str[31];
-	scanf("%s", str);
-	int depth = 1;
-	int res = 0;
-	for (int i = 0; i < strlen(str); i++) {
-
-		if (str[i] == '(' || str[i] == '[') {
-			int t = (str[i] == '(') ? 2 : 3;
-			push(str[i]);
-			depth *= t;
-		}
-		else if (str[i] == ')' || str[i] == ']') {
-			if (isEmpty()) {
-				printf("0");
+// 이 위로 수정 금지
+int calculate_vps(char* s) {
+	init_stack();
+	for (int i = 0; s[i]; i++) {
+		if (s[i] == '(' || s[i] == '[' || s[i] == '{')
+			push(s[i]);
+		else {
+			if (is_empty())
 				return 0;
-			}
-			if (str[i - 1] == '(' && str[i] == ')') {
-				res += depth;
-			}
-			else if (str[i - 1] == '[' && str[i] == ']') {
-				res += depth;
-			}
-			else if ((stack[top] == '(' && str[i] == ']') || (stack[top] == '[' && str[i] == ')')) {
-				printf("0");
+			if ((s[i] == ')' && stack[top] != '(') || (s[i] == '}' && (stack[top] != '{') || (s[i] == ']' && stack[top] != '[')))
 				return 0;
-			}
-			int t1 = (str[i] == ')') ? 2 : 3;
 			pop();
-			depth /= t1;
+		}
+	}
+	if (!is_empty())
+		return 0;
+
+	init_stack();
+	int depth = 1;
+	int val = 0;
+	int res = 0;
+	for (int i = 0; s[i]; i++) {
+		if (s[i] == '(') {
+			depth *= 2;
+			push(s[i]);
+			continue;
+		}
+		else if (s[i] == '[') {
+			depth *= 3;
+			push(s[i]);
+			continue;
+		}
+		
+
+		if (s[i] == ')') {
+			//printf("%d %d\n",depth,val);
+			pop();
+			val = depth;
+			depth /= 2;
+			if (depth == 1) {
+				if (s[i - 1] == ')' || s[i - 1] == ']') {
+
+				}
+				else res += val;
+				val = 0;
+			}
+			else {
+				if (s[i - 1] == ')' || s[i - 1] == ']') {
+
+				}
+				else res += val;
+			}
+
+			continue;
+		}
+		else if (s[i] == ']') {
+			//printf("%d %d\n",depth,val);
+			pop();
+			val = depth;
+			depth /= 3;
+			if (depth == 1) {
+				if (s[i - 1] == ')' || s[i - 1] == ']') {
+
+				}
+				else res += val;
+				val = 0;
+			}
+			else {
+				if (s[i - 1] == ')' || s[i - 1] == ']') {
+
+				}
+				else res += val;
+			}
+
+
+			continue;
 		}
 
 	}
+	return res;
+}
+int main() {
 
-	if (!isEmpty())printf("0");
-	else printf("%d", res);
+	char buf[31];
+	scanf("%s", buf);
+	printf("%d",calculate_vps(buf));
 
 	return 0;
 }
